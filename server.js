f4876f5a8bea057213fe740093716e97090fb324
SuningChen <tuning@live.unc.edu>
2023-04-27 17:48:13 -0400

    change import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import minimist from 'minimist';
 
-import { playRPS, playRPSLS } from './lib/rpsls.js';
+import { playRps, playRpsls } from './lib/rpsls.js';
 
 const app = express();
 app.use(express.json());
