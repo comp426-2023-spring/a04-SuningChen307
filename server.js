23c9de6cc62c33a996e7b91e398ee96a9b4b6379
SuningChen <tuning@live.unc.edu>
2023-04-27 17:12:58 -0400

    app

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ import express from 'express';
 import minimist from 'minimist';
 
 import { playRPS, playRPSLS } from './lib/rpsls.js';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
